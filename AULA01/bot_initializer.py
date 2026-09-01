0fc7afd21bed6da8a450f2568428ef65890d7339
Your Name <you@example.com>
2026-09-01 19:16:22 -0300

    Aula 1 robo

---
+++ AULA01/bot_initializer.py
@@ -0,0 +1,16 @@
+BOT_NAME = "MechaG"
+MAX_RETRIES = 4
+EXECUTION_TIMEOUT = 30.5
+IS_PRODUCTION = True
+
+print("INITIALIZING MECHAG BOT\t")
+print(f"ROBOT NAME: {BOT_NAME} \t(type: {type(BOT_NAME)})")
+print(f"MAX. AMOUNT OF RETRIES: {MAX_RETRIES} \t(type: {type(MAX_RETRIES)})")
+print(f"EXECUTION TIMEOUT: {EXECUTION_TIMEOUT} \t(type: {type(EXECUTION_TIMEOUT)})")
+print(f"IS PRODUCTION BOT?: {IS_PRODUCTION} \t(type: {type(IS_PRODUCTION)})")
+
+print("\nBot config loaded successfully.\t")
+
+print("\nBOT INITIALIZATION COMPLETE")
+
+#Feito por Arthur Bittar
